c63748132367b4f5b81eb651ca22d70d6a6cb15b
Teuku Rijal <tm.rijalul@mifx.com>
2023-09-05 18:32:56 +0700

    feat: add sample video

---
+++ src/components/background-video/index.js
@@ -0,0 +1,9 @@
+export default function BackgroundVideo() {
+    return (
+      <div>
+        <video autoPlay loop >
+          <source src="/video/landing_video.mp4" type="video/mp4" />
+        </video>
+      </div>
+    )
+}
