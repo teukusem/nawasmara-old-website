f02be7ce5144c5fc4d97fd9049a4b8ea815e8df6
Teuku Rijal <tm.rijalul@mifx.com>
2023-09-10 15:25:17 +0700

    fix: unmuted video

--- src/components/background-video/index.js
+++ src/components/background-video/index.js
@@ -2,19 +2,19 @@ import { useEffect } from "react";
 
 export default function BackgroundVideo() {
 
-  useEffect(() => {
-    document.addEventListener(
-      "click",
-      () => {
-        document.getElementById("backgroundMusic").play();
-      },
-      { once: true }
-    );
-  });
+  // useEffect(() => {
+  //   document.addEventListener(
+  //     "click",
+  //     () => {
+  //       document.getElementById("backgroundMusic").play();
+  //     },
+  //     { once: true }
+  //   );
+  // });
 
   return (
     <div className="z-1">
-      <video autoPlay loop playsinline className="video" id="backgroundMusic">
+      <video autoPlay loop muted playsinline className="video" id="backgroundMusic">
         <source src="/video/landing_video.mp4" type="video/mp4" />
       </video>
       <style jsx>{`
