78867b2ba81c91eb7084b1e18de6cf25341b2b61
Teuku Rijal <tm.rijalul@mifx.com>
2023-09-23 15:08:49 +0700

    wip: home page

--- src/components/background-video/index.js
+++ src/components/background-video/index.js
@@ -1,11 +1,11 @@
 export default function BackgroundVideo() {
-  const html = `<video id="backgroundMusic" src="/video/landing_video.mp4" classname="video" autoplay loop muted playsinline><style jsx></style>`;
+  const html = `<video id="backgroundMusic" src="/video/landing_video.mp4" classname="video" autoplay loop muted playsinline>`;
   return (
     <div
-      className="z-1"
+      className="z-1 flex justify-center h-screen items-center"
       dangerouslySetInnerHTML={{
         __html: html,
       }}
-    ></div>
+    />
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,16 @@
 import Head from "next/head";
-import { useEffect } from "react";
-import { useGlobalAudioPlayer } from "react-use-audio-player";
-
-import BackgroundVideo from "@/components/background-video";
 import { data } from "@/constants/ariefnabila";
+import LandingPage from "@/components/landing-page";
 
 export default function Home() {
-  const { load } = useGlobalAudioPlayer();
   const { title } = data;
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      load("/audio/soundtrack.mp3", {
-        autoplay: true,
-      });
-    }, 1000);
-    return () => clearTimeout(timer);
-  }, []);
 
   return (
     <>
       <Head>
         <title>{title}</title>
       </Head>
-      <div>
-        <BackgroundVideo id="backgroundMusic" />
-      </div>
+      <LandingPage />
     </>
   );
 }
