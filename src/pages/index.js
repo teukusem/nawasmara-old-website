c4a12d7198edb104a903f6a814d20a482bc13fd0
Teuku Rijal <tm.rijalul@mifx.com>
2023-11-08 18:53:09 +0700

    fix: error build

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,25 +4,25 @@ export default function Maintenance() {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
       <Head>
-        <title>Maintenance Mode</title>
+        <title>{`Maintenance Mode`}</title>
       </Head>
       <div className="text-center">
         <h1 className="text-4xl font-bold text-gray-800 mb-4">
-          We'll be back soon!
+          {`We'll be back soon!`}
         </h1>
         <p className="text-lg text-gray-600">
-          Sorry for the inconvenience but we're performing some maintenance at
+          {`Sorry for the inconvenience but we're performing some maintenance at
           the moment.
-          <br />
-          If you need to, you can always{" "}
+          <br />`}
+          {`If you need to, you can always `}
           <a
             href="https://wa.link/926698"
             target="_blank"
             className="text-blue-600 hover:underline"
           >
-            contact us
+            {`contact us`}
           </a>
-          , otherwise, we'll be back online shortly!
+          {`, otherwise, we'll be back online shortly!`}
         </p>
       </div>
     </div>
