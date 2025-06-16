434d97e17ee148f9c227913f3ed1241d3a113918
Teuku Rijal <teukurijal89@gmail.com>
2025-06-16 22:45:30 +0700

    feat: adjust middleware mathcer

--- src/middleware.js
+++ src/middleware.js
@@ -1,17 +1,18 @@
 import { NextResponse } from 'next/server'
 
-// export const config = {
-//   matcher: [
-//     /*
-//      * Match all request paths except for the ones starting with:
-//      * - api (API routes)
-//      * - _next/static (static files)
-//      * - _next/image (image optimization files)
-//      * - favicon.ico (favicon file)
-//      */
-//     '/((?!api|_next/static|_next/image|favicon.ico).*)',
-//   ],
-// }
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - public assets
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico|.*\\.).*)',
+  ],
+}
 
 export function middleware(req) {
   const host = req.headers.get('host') || ''
