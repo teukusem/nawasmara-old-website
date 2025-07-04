1638ad719bdc79f78482784efd5d3d1ea806d233
Teuku Rijal <teukurijal89@gmail.com>
2025-07-04 15:02:20 +0700

    fix: add X-Frame-Options headers

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
